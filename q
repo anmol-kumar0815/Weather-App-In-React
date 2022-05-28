5591d31a28367b7f40c80384a24d76100be62a94
Anmol Kumar Shrivastav <anmolshrivastav.08@gmail.com>
2022-05-28 18:28:27 +0530

    Weather App Using Function Components and Hooks

---
+++ q
@@ -0,0 +1,11 @@
+[33mcommit 64fa6fcbdbbc5f38af63ba7edf76e6a11ded9090[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Anmol Kumar Shrivastav <anmolshrivastav.08@gmail.com>
+Date:   Sat May 28 17:38:28 2022 +0530
+
+    App with class component v1
+
+[33mcommit f6b8fd2a0780d80c29de60b8b76def06a22ace08[m
+Author: Anmol Kumar Shrivastav <anmolshrivastav.08@gmail.com>
+Date:   Thu May 26 21:20:57 2022 +0530
+
+    Initialize project using Create React App
--- src/myComponents/resultContainer.js
+++ src/myComponents/resultContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import axios from 'axios';
 import './style/resultContainer.css';
 
@@ -8,58 +8,50 @@ function toCelsius(kelvin) {
 }
 
 
-export default class ResultContainer extends React.Component{
+function ResultContainer(props){
 
-    constructor(props){
-        super(props);
+    const[error, setError] = useState(null);
+    const[isLoaded, setLoaded] = useState(false);
+    const[cityData, setCityData] = useState(null);
 
-        this.state = {
-            error : null,
-            isLoaded: false,
-            cityData: null
+    useEffect(()=>{
+        let city  = props.city.trim();
+        if(city !== ''){
+            console.log("City is not an empty string going to call api");
+            apiCall(props.city);
         }
-    }
+    },[props.city]);
 
-    // console.log("top of the page, city = " + props.city + " error = " + error + " isLoaded = " + isLoaded);
-    apiCall(city){
+    function apiCall(city){
         axios.get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=a1d7b9ef6a211e7dbc71a77562c5bbab").then((data)=>{
-            this.setState({error: null, isLoaded: true, cityData: data.data});
+            setError(null);
+            setLoaded(true);
+            setCityData(data.data);
         }).catch((error)=>{
-            // console.log(error.response.data.cod);
-            this.setState({error: error, isLoaded: true, cityData: null});
+            setError(error);
+            setLoaded(true);
         });
     }
 
-    componentDidMount(){
-        this.apiCall(this.props.city);
-    }
-
-    componentDidUpdate(prevProps){
-        if(prevProps.city !== this.props.city){
-            this.apiCall(this.props.city);
-        }
-    }
-
-    render(){
-        if(this.state.error !== null){
-            if(this.state.error.response.data.cod === "404"){
-                return <div className="result-container">Error = City Not Found, Please Enter a valid City Name.</div>
-            }else{
-                return <div className="result-container">Error = {this.state.error.message}</div>
-            }
-        }else if(this.state.isLoaded === false){
-            return <div className="result-container">Loading....</div>
+    if(error !== null){
+        if(error.response.data.cod === "404"){
+            return <div className="result-container">Error = City Not Found, Please Enter a valid City Name.</div>
         }else{
-            return(
-                <div className="result-container">
-                    <p className="city-description">Temperature = {toCelsius(this.state.cityData.main.temp)} °C</p>
-                    <p className="city-description">Weather Description = {this.state.cityData.weather[0].main}</p>
-                    <p className="city-description">Humidity = {this.state.cityData.main.humidity}</p>
-                    <p className="city-description">Visibility = {this.state.cityData.visibility} Meters</p>
-                    <p className="city-description">Wind Speed = {this.state.cityData.wind.speed} Km/Hour</p>
-                </div>
-            );
+            return <div className="result-container">Error = {error.message}</div>
         }
+    }else if(isLoaded === false){
+        return <div className="result-container">Loading....</div>
+    }else{
+        return(
+            <div className="result-container">
+                <p className="city-description">Temperature = {toCelsius(cityData.main.temp)} °C</p>
+                <p className="city-description">Weather Description = {cityData.weather[0].main}</p>
+                <p className="city-description">Humidity = {cityData.main.humidity}</p>
+                <p className="city-description">Visibility = {cityData.visibility} Meters</p>
+                <p className="city-description">Wind Speed = {cityData.wind.speed} Km/Hour</p>
+            </div>
+        );
     }
-    
 }
+
+export default ResultContainer;
